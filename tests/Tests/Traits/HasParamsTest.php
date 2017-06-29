b950542bdc945dae1be0a6c1cfb21f8d418fda84
Roberto Segura - phproberto <roberto@phproberto.com>
2017-06-29 08:49:49 +0200

    [test] HasParams trait tests

---
+++ tests/Tests/Traits/HasParamsTest.php
@@ -0,0 +1,215 @@
+<?php
+/**
+ * Joomla! entity library.
+ *
+ * @copyright  Copyright (C) 2017 Roberto Segura López, Inc. All rights reserved.
+ * @license    See COPYING.txt
+ */
+
+namespace Phproberto\Joomla\Entity\Tests\Traits;
+
+use Phproberto\Joomla\Entity\Tests\Traits\Stubs\EntityWithParams;
+use Joomla\Registry\Registry;
+
+/**
+ * HasParams trait tests.
+ *
+ * @since   __DEPLOY_VERSION__
+ */
+class HasParamsTest extends \PHPUnit\Framework\TestCase
+{
+	/**
+	 * getParam returns correct value.
+	 *
+	 * @return  void
+	 */
+	public function testGetParamReturnsCorrectValue()
+	{
+		$entity = new EntityWithParams;
+
+		$reflection = new \ReflectionClass($entity);
+		$rowProperty = $reflection->getProperty('row');
+		$rowProperty->setAccessible(true);
+
+		$rowProperty->setValue($entity, ['id' => 999, 'params' => '{"foo":"var"}']);
+
+		$this->assertSame('var', $entity->getParam('foo'));
+		$this->assertSame(null, $entity->getParam('unknown'));
+		$this->assertSame('default', $entity->getParam('use-default', 'default'));
+	}
+
+	/**
+	 * getParams works with attribs column.
+	 *
+	 * @return  void
+	 */
+	public function testGetParamsWorksWithAttribsColumn()
+	{
+		$mock = $this->getMockBuilder(EntityWithParams::class)
+			->setMethods(array('getParamsColumn'))
+			->getMock();
+
+		$mock->expects($this->once())
+			->method('getParamsColumn')
+			->willReturn('attribs');
+
+		$reflection = new \ReflectionClass($mock);
+		$rowProperty = $reflection->getProperty('row');
+		$rowProperty->setAccessible(true);
+
+		$rowProperty->setValue($mock, ['id' => 999, 'attribs' => '{"foo":"var"}']);
+
+		$this->assertEquals(new Registry(['foo' => 'var']), $mock->getParams());
+	}
+
+	/**
+	 * getParams works with params column.
+	 *
+	 * @return  void
+	 */
+	public function testGetParamsWorksWithParamsColumn()
+	{
+		$entity = new EntityWithParams;
+
+		$reflection = new \ReflectionClass($entity);
+		$rowProperty = $reflection->getProperty('row');
+		$rowProperty->setAccessible(true);
+
+		$rowProperty->setValue($entity, ['id' => 999, 'params' => '{"foo":"bar"}']);
+
+		$this->assertEquals(new Registry(['foo' => 'bar']), $entity->getParams());
+	}
+
+	/**
+	 * setParam sets the correct param value.
+	 *
+	 * @return  void
+	 */
+	public function testSetParamSetsCorrectParamValue()
+	{
+		$entity = new EntityWithParams(999);
+
+		$reflection = new \ReflectionClass($entity);
+		$rowProperty = $reflection->getProperty('row');
+		$rowProperty->setAccessible(true);
+
+		$rowProperty->setValue($entity, ['id' => 999, 'params' => '{"test":"var"}']);
+
+		$reflection = new \ReflectionClass($entity);
+		$paramsProperty = $reflection->getProperty('params');
+		$paramsProperty->setAccessible(true);
+
+		$this->assertSame(null, $paramsProperty->getValue($entity));
+
+		$entity->setParam('foo', 'foobar');
+
+		$this->assertEquals(new Registry(['test' => 'var', 'foo' => 'foobar']), $entity->getParams());
+
+		$entity->setParam('test', 'modified-var');
+
+		$expectedParams = new Registry(['test' => 'modified-var', 'foo' => 'foobar']);
+
+		$this->assertEquals($expectedParams, $entity->getParams());
+		$this->assertEquals($expectedParams->toString(), $rowProperty->getValue($entity)['params']);
+	}
+
+	/**
+	 * setParam updates row parameters.
+	 *
+	 * @return  void
+	 */
+	public function testSetParamSetsCorrectParamValueWithCustomParamsColumn()
+	{
+		$entity = $this->getMockBuilder(EntityWithParams::class)
+			->setMethods(array('getParamsColumn'))
+			->getMock();
+
+		$entity->method('getParamsColumn')
+			->willReturn('attribs');
+
+		$reflection = new \ReflectionClass($entity);
+		$rowProperty = $reflection->getProperty('row');
+		$rowProperty->setAccessible(true);
+
+		$rowProperty->setValue($entity, ['id' => 999, 'attribs' => '{"test":"var"}']);
+
+		$reflection = new \ReflectionClass($entity);
+		$paramsProperty = $reflection->getProperty('params');
+		$paramsProperty->setAccessible(true);
+
+		$this->assertSame(null, $paramsProperty->getValue($entity));
+
+		$entity->setParam('foo', 'foobar');
+
+		$this->assertEquals(new Registry(['test' => 'var', 'foo' => 'foobar']), $paramsProperty->getValue($entity));
+
+		$entity->setParam('test', 'modified-var');
+
+		$expectedParams = new Registry(['test' => 'modified-var', 'foo' => 'foobar']);
+
+		$this->assertEquals($expectedParams, $paramsProperty->getValue($entity));
+		$this->assertEquals($expectedParams->toString(), $rowProperty->getValue($entity)['attribs']);
+	}
+
+	/**
+	 * setParam sets the correct param value.
+	 *
+	 * @return  void
+	 */
+	public function testSetParamsSetsCorrectValue()
+	{
+		$entity = new EntityWithParams(999);
+
+		$reflection = new \ReflectionClass($entity);
+		$rowProperty = $reflection->getProperty('row');
+		$rowProperty->setAccessible(true);
+
+		$rowProperty->setValue($entity, ['id' => 999, 'params' => '{"test":"var"}']);
+
+		$reflection = new \ReflectionClass($entity);
+		$paramsProperty = $reflection->getProperty('params');
+		$paramsProperty->setAccessible(true);
+
+		$this->assertSame(null, $paramsProperty->getValue($entity));
+
+		$expectedParams = new Registry(['test' => 'modified-var', 'foo' => 'foobar']);
+		$entity->setParams($expectedParams);
+
+		$this->assertEquals($expectedParams, $paramsProperty->getValue($entity));
+		$this->assertEquals($expectedParams->toString(), $rowProperty->getValue($entity)['params']);
+	}
+
+	/**
+	 * setParam updates row parameters.
+	 *
+	 * @return  void
+	 */
+	public function testSetParamsSetsCorrectValueWithCustomParamsColumn()
+	{
+		$entity = $this->getMockBuilder(EntityWithParams::class)
+			->setMethods(array('getParamsColumn'))
+			->getMock();
+
+		$entity->method('getParamsColumn')
+			->willReturn('attribs');
+
+		$reflection = new \ReflectionClass($entity);
+		$rowProperty = $reflection->getProperty('row');
+		$rowProperty->setAccessible(true);
+
+		$rowProperty->setValue($entity, ['id' => 999, 'attribs' => '{"test":"var"}']);
+
+		$reflection = new \ReflectionClass($entity);
+		$paramsProperty = $reflection->getProperty('params');
+		$paramsProperty->setAccessible(true);
+
+		$this->assertSame(null, $paramsProperty->getValue($entity));
+
+		$expectedParams = new Registry(['test' => 'modified-var', 'foo' => 'foobar']);
+
+		$entity->setParams($expectedParams);
+
+		$this->assertEquals($expectedParams, $paramsProperty->getValue($entity));
+		$this->assertEquals($expectedParams->toString(), $rowProperty->getValue($entity)['attribs']);
+	}
+}
