b7da63610cef1718aeeefa26ef927f7554910d84
Roberto Segura - phproberto <roberto@phproberto.com>
2017-07-14 11:32:54 +0200

    [imp] trait for entities with client

---
+++ src/Core/Traits/HasClient.php
@@ -0,0 +1,75 @@
+<?php
+/**
+ * Joomla! common library.
+ *
+ * @copyright  Copyright (C) 2017 Roberto Segura López, Inc. All rights reserved.
+ * @license    GNU/GPL 2, http://www.gnu.org/licenses/gpl-2.0.htm
+ */
+
+namespace Phproberto\Joomla\Entity\Core\Traits;
+
+use Phproberto\Joomla\Entity\Core\Asset;
+use Phproberto\Joomla\Client\Client;
+use Phproberto\Joomla\Client\ClientInterface;
+
+defined('JPATH_PLATFORM') || die;
+
+/**
+ * Trait for entities that have a client assigned.
+ *
+ * @since  __DEPLOY_VERSION__
+ */
+trait HasClient
+{
+	/**
+	 * Associated client.
+	 *
+	 * @var  ClientInterface
+	 */
+	protected $client;
+
+	/**
+	 * Get the name of the column that stores category.
+	 *
+	 * @return  string
+	 */
+	protected function columnClient()
+	{
+		return 'client_id';
+	}
+
+	/**
+	 * Get the associated client.
+	 *
+	 * @param   boolean  $reload  Force reloading
+	 *
+	 * @return  ClientInterface
+	 */
+	public function client($reload = false)
+	{
+		if ($reload || null === $this->client)
+		{
+			$this->client = $this->loadClient();
+		}
+
+		return $this->client;
+	}
+
+	/**
+	 * Load the client from the database.
+	 *
+	 * @return  Category
+	 */
+	protected function loadClient()
+	{
+		$column = $this->columnClient();
+		$data = $this->all();
+
+		if (!array_key_exists($column, $data))
+		{
+			throw new \RuntimeException(__CLASS__ . ": Cannot load entity client");
+		}
+
+		return (int) $data[$column] ? Client::admin() : Client::site();
+	}
+}
---
+++ tests/Tests/Core/Traits/HasClientTest.php
@@ -0,0 +1,139 @@
+<?php
+/**
+ * Joomla! entity library.
+ *
+ * @copyright  Copyright (C) 2017 Roberto Segura López, Inc. All rights reserved.
+ * @license    See COPYING.txt
+ */
+
+namespace Phproberto\Joomla\Entity\Tests\Core\Traits;
+
+use Phproberto\Joomla\Client\Administrator;
+use Phproberto\Joomla\Client\Site;
+use Phproberto\Joomla\Entity\Tests\Core\Traits\Stubs\ClassWithClient;
+
+/**
+ * HasClient trait tests.
+ *
+ * @since   __DEPLOY_VERSION__
+ */
+class HasClientTest extends \PHPUnit\Framework\TestCase
+{
+	/**
+	 * Column storing the client identifier.
+	 *
+	 * @const
+	 */
+	const CLIENT_COLUMN = 'client_id';
+
+	/**
+	 * Tears down the fixture, for example, closes a network connection.
+	 * This method is called after a test is executed.
+	 *
+	 * @return  void
+	 */
+	protected function tearDown()
+	{
+		ClassWithClient::clearAllInstances();
+
+		parent::tearDown();
+	}
+
+	/**
+	 * getClient returns correct data.
+	 *
+	 * @return  void
+	 */
+	public function testGetClientReturnsCorrectData()
+	{
+		$class = new ClassWithClient;
+
+		$reflection = new \ReflectionClass($class);
+		$rowProperty = $reflection->getProperty('row');
+		$rowProperty->setAccessible(true);
+
+		$rowProperty->setValue($class, array('id' => 999, self::CLIENT_COLUMN => 0));
+
+		$this->assertInstanceOf(Site::class, $class->client());
+
+		$rowProperty->setValue($class, array('id' => 999, self::CLIENT_COLUMN => 1));
+
+		// Without force it should return cached client
+		$this->assertInstanceOf(Site::class, $class->client());
+		$this->assertInstanceOf(Administrator::class, $class->client(true));
+	}
+
+	/**
+	 * client throws an exception when client column is not found.
+	 *
+	 * @return  void
+	 *
+	 * @expectedException  \RuntimeException
+	 */
+	public function testLoadClientThrowsExceptionWhenClientColumnIsNotFound()
+	{
+		$class = new ClassWithClient;
+
+		$reflection = new \ReflectionClass($class);
+		$method = $reflection->getMethod('loadClient');
+		$method->setAccessible(true);
+		$rowProperty = $reflection->getProperty('row');
+		$rowProperty->setAccessible(true);
+
+		$rowProperty->setValue($class, array('id' => 999));
+
+		$method->invoke($class);
+	}
+
+	/**
+	 * loadClient returns correct value.
+	 *
+	 * @return  void
+	 */
+	public function testLoadClientReturnsCorrectValue()
+	{
+		$class = new ClassWithClient(999);
+
+		$reflection = new \ReflectionClass($class);
+		$method = $reflection->getMethod('loadClient');
+		$method->setAccessible(true);
+		$rowProperty = $reflection->getProperty('row');
+		$rowProperty->setAccessible(true);
+
+		$rowProperty->setValue($class, array('id' => 999, 'client_id' => 0));
+
+		$this->assertInstanceOf(Site::class, $method->invoke($class));
+
+		$rowProperty->setValue($class, array('id' => 999, 'client_id' => '0'));
+
+		$this->assertInstanceOf(Site::class, $method->invoke($class));
+
+		$rowProperty->setValue($class, array('id' => 999, 'client_id' => 'thiswillreturn0'));
+
+		$this->assertInstanceOf(Site::class, $method->invoke($class));
+
+		$rowProperty->setValue($class, array('id' => 999, 'client_id' => 1));
+
+		$this->assertInstanceOf(Administrator::class, $method->invoke($class));
+
+		$rowProperty->setValue($class, array('id' => 999, 'client_id' => '1'));
+
+		$this->assertInstanceOf(Administrator::class, $method->invoke($class));
+	}
+
+	/**
+	 * columnClient returns correct value.
+	 *
+	 * @return  void
+	 */
+	public function testColumnClientReturnsCorrectValue()
+	{
+		$class = new ClassWithClient;
+
+		$reflection = new \ReflectionClass($class);
+		$method = $reflection->getMethod('columnClient');
+		$method->setAccessible(true);
+
+		$this->assertEquals(self::CLIENT_COLUMN, $method->invoke($class));
+	}
+}
