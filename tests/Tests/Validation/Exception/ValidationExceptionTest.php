2f509592d3f4f634bdaadca100fecb48b6c37c1c
Roberto Segura - phproberto <roberto@phproberto.com>
2017-09-08 13:19:56 +0200

    [imp] exceptions tests

---
+++ tests/Tests/Validation/Exception/ValidationExceptionTest.php
@@ -0,0 +1,59 @@
+<?php
+/**
+ * Joomla! entity library.
+ *
+ * @copyright  Copyright (C) 2017 Roberto Segura López, Inc. All rights reserved.
+ * @license    See COPYING.txt
+ */
+
+namespace Phproberto\Joomla\Entity\Tests\Validation\Exception;
+
+use Phproberto\Joomla\Entity\Tests\Stubs\Entity;
+use Phproberto\Joomla\Entity\Validation\Rule;
+use Phproberto\Joomla\Entity\Validation\Exception\ValidationException;
+
+/**
+ * ValidationException tests.
+ *
+ * @since   __DEPLOY_VERSION__
+ */
+class ValidationExceptionTest extends \TestCase
+{
+	/**
+	 * invalidEntity returns ValidationException.
+	 *
+	 * @return  void
+	 */
+	public function testInvalidEntityReturnsValidationException()
+	{
+		$entity = new Entity(999);
+		$errors = array(
+			'`alias` cannot be empty',
+			'`column` is wrong'
+		);
+
+		$exception = ValidationException::invalidEntity($entity, $errors);
+
+		$this->assertInstanceOf(ValidationException::class, $exception);
+		$this->assertTrue(strlen($exception->getMessage()) > 0);
+	}
+
+	/**
+	 * invalidColumn returns ValidationException.
+	 *
+	 * @return  void
+	 */
+	public function testInvalidColumnReturnsValidationException()
+	{
+		$entity = new Entity(999);
+		$failedRules = array(
+			new Rule\IsPositiveInteger('Is not a positive integer'),
+			new Rule\IsNotEmptyString('Cannot be an empty string')
+		);
+
+		$exception = ValidationException::invalidColumn('sample_column', $failedRules);
+
+		$this->assertInstanceOf(ValidationException::class, $exception);
+		$this->assertTrue(strlen($exception->getMessage()) > 0);
+	}
+}
