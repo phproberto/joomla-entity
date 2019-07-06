1aa64eb2247b04cfd1702a9e5e778a48c0bc6f82
Roberto Segura - phproberto <roberto@phproberto.com>
2019-07-06 18:36:55 +0200

    [imp] DropTable command

---
+++ src/Command/Database/DropTable.php
@@ -0,0 +1,70 @@
+<?php
+/**
+ * Joomla! entity library.
+ *
+ * @copyright  Copyright (C) 2017-2019 Roberto Segura López, Inc. All rights reserved.
+ * @license    See COPYING.txt
+ */
+
+namespace Phproberto\Joomla\Entity\Command\Database;
+
+defined('_JEXEC') || die;
+
+use Joomla\CMS\Factory;
+use Phproberto\Joomla\Entity\Command\BaseCommand;
+use Phproberto\Joomla\Entity\Command\Contracts\CommandInterface;
+
+/**
+ * Drop a database table.
+ *
+ * @since  __DEPLOY_VERSION__
+ */
+final class DropTable extends BaseCommand implements CommandInterface
+{
+	/**
+	 * Database driver.
+	 *
+	 * @var  \JDatabaseDriver
+	 */
+	private $db;
+
+	/**
+	 * Name of the table to drop.
+	 *
+	 * @var  string
+	 */
+	private $tableName;
+
+	/**
+	 * Constructor.
+	 *
+	 * @param   string  $name     Name of the table to drop.
+	 * @param   array   $options  Additional settings
+	 */
+	public function __construct(string $name, array $options = [])
+	{
+		$this->tableName = $name;
+		$this->db = isset($options['db']) ? $options['db'] : Factory::getDbo();
+
+		unset($options['db']);
+
+		parent::__construct($options);
+	}
+
+	/**
+	 * Execute the command.
+	 *
+	 * @return  mixed
+	 */
+	public function execute()
+	{
+		try
+		{
+			$result = $this->db->dropTable($this->tableName);
+		}
+		catch (\RuntimeException $e)
+		{
+			throw new \RuntimeException(sprintf('Error dropping DB table `%s`: %s', $this->tableName, $e->getMessage()));
+		}
+	}
+}
---
+++ tests/tests/Command/Database/DropTableTest.php
@@ -0,0 +1,141 @@
+<?php
+/**
+ * Joomla! entity library.
+ *
+ * @copyright  Copyright (C) 2017-2018 Roberto Segura López, Inc. All rights reserved.
+ * @license    See COPYING.txt
+ */
+
+namespace Phproberto\Joomla\Entity\Tests\Command\Database;
+
+defined('_JEXEC') || die;
+
+use Joomla\CMS\Factory;
+use Phproberto\Joomla\Entity\Command\Database\DropTable;
+
+/**
+ * Base command tests.
+ *
+ * @since   __DEPLOY_VERSION__
+ */
+class DropTableTest extends \TestCaseDatabase
+{
+	/**
+	 * @test
+	 *
+	 * @return void
+	 */
+	public function tableIsDropped()
+	{
+		static::$driver->setQuery(
+			'CREATE TABLE `droptable_test` (
+				`id` INTEGER PRIMARY KEY NOT NULL
+			)'
+		);
+
+		static::$driver->execute();
+		Factory::$database = static::$driver;
+		$db = Factory::getDbo();
+
+		$this->assertTrue(in_array('droptable_test', $db->getTableList(), true));
+
+		$command = new DropTable('droptable_test');
+		$command->execute();
+
+		$this->assertFalse(in_array('droptable_test', $db->getTableList(), true));
+	}
+
+	/**
+	 * @test
+	 *
+	 * @return void
+	 */
+	public function exceptionIsThrownOnError()
+	{
+		$db2 = \JDatabaseDriver::getInstance(
+			[
+				'driver' => 'mysqli',
+				'database' => 'test',
+				'prefix' => 'ddd'
+			]
+		);
+
+		$error = '';
+
+		try
+		{
+			$command = new DropTable('inexistent', ['db' => $db2]);
+			$command->execute();
+		}
+		catch (\RuntimeException $e)
+		{
+			$error = $e->getMessage();
+		}
+
+		$this->assertSame('Error dropping DB table `inexistent`: Could not connect to MySQL server.', $error);
+	}
+
+	/**
+	 * @test
+	 *
+	 * @return void
+	 */
+	public function customDriverIsUsed()
+	{
+		$db2 = \JDatabaseDriver::getInstance(
+			[
+				'driver' => 'sqlite',
+				'database' => ':droptable_test:',
+				'prefix' => 'ddd'
+			]
+		);
+
+		$db2->setQuery(
+			'CREATE TABLE `droptable_test2` (
+				`id` INTEGER PRIMARY KEY NOT NULL
+			)'
+		);
+		$db2->execute();
+
+		$this->assertTrue(in_array('droptable_test2', $db2->getTableList(), true));
+
+		$command = new DropTable('droptable_test2', ['db' => $db2]);
+		$command->execute();
+
+		$this->assertFalse(in_array('droptable_test2', $db2->getTableList(), true));
+
+		$db2->disconnect();
+
+		unlink(dirname(JPATH_TESTS_PHPROBERTO) . '/:droptable_test:');
+	}
+
+	/**
+	 * Sets up the fixture, for example, opens a network connection.
+	 * This method is called before a test is executed.
+	 *
+	 * @return  void
+	 */
+	protected function setUp()
+	{
+		parent::setUp();
+
+		$this->saveFactoryState();
+
+		Factory::$config      = $this->getMockConfig();
+		Factory::$application = $this->getMockCmsApp();
+		Factory::$session     = $this->getMockSession();
+	}
+
+	/**
+	 * Tears down the fixture, for example, closes a network connection.
+	 * This method is called after a test is executed.
+	 *
+	 * @return  void
+	 */
+	protected function tearDown()
+	{
+		$this->restoreFactoryState();
+
+		parent::tearDown();
+	}
+}
