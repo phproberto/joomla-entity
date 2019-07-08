1c0625b5a01a0662ab0417b5227bb7e7f3b264f1
Roberto Segura - phproberto <roberto@phproberto.com>
2019-07-08 09:35:03 +0200

    [imp] command to delete a folder recursively

---
+++ src/Command/FileSystem/DeleteFolderRecursively.php
@@ -0,0 +1,83 @@
+<?php
+/**
+ * Joomla! entity library.
+ *
+ * @copyright  Copyright (C) 2017-2019 Roberto Segura López, Inc. All rights reserved.
+ * @license    See COPYING.txt
+ */
+
+namespace Phproberto\Joomla\Entity\Command\FileSystem;
+
+defined('_JEXEC') || die;
+
+use Phproberto\Joomla\Entity\Command\BaseCommand;
+use Phproberto\Joomla\Entity\Command\Contracts\CommandInterface;
+
+/**
+ * Delete a folder and all its contents.
+ *
+ * @since  __DEPLOY_VERSION__
+ */
+class DeleteFolderRecursively extends BaseCommand implements CommandInterface
+{
+	/**
+	 * Folder to delete.
+	 *
+	 * @var  string
+	 */
+	private $folder;
+
+	/**
+	 * Constructor.
+	 *
+	 * @param   string  $folder   Folder to delete
+	 * @param   array   $options  Array with other options
+	 */
+	public function __construct(string $folder, array $options = [])
+	{
+		$this->folder = trim($folder);
+
+		parent::__construct($options);
+	}
+
+	/**
+	 * Execute the command.
+	 *
+	 * @return  boolean
+	 */
+	public function execute()
+	{
+		if (!is_dir($this->folder))
+		{
+			return true;
+		}
+
+		$files = glob($this->folder . '/*');
+
+		foreach ($files as $file)
+		{
+			if (is_dir($file))
+			{
+				self::instance([$file])->execute();
+
+				continue;
+			}
+
+			if (!@unlink($file))
+			{
+				$error = sprintf("Error deleting file: `%s`", $file);
+
+				throw new \RuntimeException($error);
+			}
+		}
+
+		if (!rmdir($this->folder))
+		{
+			$error = sprintf("Error deleting folder: `%s`", $this->folder);
+
+			throw new \RuntimeException($error);
+		}
+
+		return true;
+	}
+}
---
+++ tests/tests/Command/FileSystem/DeleteFolderRecursivelyTest.php
@@ -0,0 +1,101 @@
+<?php
+/**
+ * Joomla! entity library.
+ *
+ * @copyright  Copyright (C) 2017-2018 Roberto Segura López, Inc. All rights reserved.
+ * @license    See COPYING.txt
+ */
+
+namespace Phproberto\Joomla\Entity\Tests\Command\FileSystem;
+
+defined('_JEXEC') || die;
+
+use Joomla\CMS\Factory;
+use Phproberto\Joomla\Entity\Command\FileSystem\DeleteFolderRecursively;
+
+/**
+ * DeleteFolderRecursivelyTest tests.
+ *
+ * @since   __DEPLOY_VERSION__
+ */
+class DeleteFolderRecursivelyTestTest extends \TestCase
+{
+	/**
+	 * Create a test folder structure.
+	 *
+	 * @return  void
+	 */
+	private function createTestFolder()
+	{
+		$tmpFolder = $this->tmpFolder();
+
+		if (is_dir($tmpFolder))
+		{
+			$this->deleteTestFolder();
+		}
+
+		$childFolder = $tmpFolder . '/child-folder';
+
+		mkdir($tmpFolder);
+		touch($tmpFolder . '/delete-me.txt');
+		mkdir($childFolder);
+		touch($childFolder . '/delete-me-too.txt');
+	}
+
+	/**
+	 * Delete the test folder.
+	 *
+	 * @return  void
+	 */
+	private function deleteTestFolder()
+	{
+		$tmpFolder = $this->tmpFolder();
+		$childFolder = $tmpFolder . '/child-folder';
+
+		@unlink($tmpFolder . '/delete-me.txt');
+		@unlink($childFolder . '/delete-me-too.txt');
+		@rmdir($childFolder);
+		@rmdir($tmpFolder);
+	}
+
+	/**
+	 * @test
+	 *
+	 * @return void
+	 */
+	public function deletingUnexistingFolderReturnsTrue()
+	{
+		$this->assertTrue(
+			DeleteFolderRecursively::instance([__DIR__ . '/does-not-exist'])->execute()
+		);
+	}
+
+	/**
+	 * @test
+	 *
+	 * @return void
+	 */
+	public function folderIsDeleted()
+	{
+		$this->createTestFolder();
+
+		$tmpFolder = $this->tmpFolder();
+
+		$this->assertTrue(is_dir($tmpFolder));
+
+		$command = new DeleteFolderRecursively($tmpFolder);
+		$command->execute();
+
+		$this->assertFalse(is_dir($tmpFolder));
+	}
+
+	/**
+	 * Route to the temporary folder used to test this command.
+	 *
+	 * @return  string
+	 */
+	private function tmpFolder()
+	{
+		return __DIR__ . '/tmp';
+	}
+}
