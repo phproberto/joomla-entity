6d111be5d5435d025d7dde52cffdd3edea353afa
Roberto Segura - phproberto <roberto@phproberto.com>
2019-09-16 10:27:49 +0200

    [imp] new helper to deal with class names/namespaces stuff

---
+++ src/Helper/ClassName.php
@@ -0,0 +1,72 @@
+<?php
+/**
+ * Joomla! entity library.
+ *
+ * @copyright  Copyright (C) 2017-2019 Roberto Segura López, Inc. All rights reserved.
+ * @license    See COPYING.txt
+ */
+
+namespace Phproberto\Joomla\Entity\Helper;
+
+defined('_JEXEC') || die;
+
+use Phproberto\Joomla\Entity\Contracts\EntityInterface;
+use Phproberto\Joomla\Entity\Contracts\ExceptionInterface;
+
+/**
+ * Helper to deal with class name/namespaces things.
+ *
+ * @since  __DEPLOY_VERSION__
+ */
+abstract class ClassName
+{
+	/**
+	 * Check if a class uses namespace.
+	 *
+	 * @param   object  $class  Class instance
+	 *
+	 * @return  boolean
+	 */
+	public function inNamespace($class): bool
+	{
+		return (new \ReflectionClass($class))->inNamespace();
+	}
+
+	/**
+	 * Retrieve class namespace.
+	 *
+	 * @param   object  $class  Class instance
+	 *
+	 * @return  string
+	 */
+	public static function namespace($class): string
+	{
+		return (new \ReflectionClass($class))->getNamespaceName();
+	}
+
+	/**
+	 * Guess the entity class
+	 *
+	 * @param   object  $class  Class instance
+	 *
+	 * @return  string
+	 */
+	public static function parentNamespace($class): string
+	{
+		$namespace = self::namespace($class);
+
+		return substr($namespace, 0, strrpos($namespace, '\\'));
+	}
+
+	/**
+	 * Retrieve class namespace without namespace.
+	 *
+	 * @param   object  $class  Class instance
+	 *
+	 * @return  string
+	 */
+	public function withoutNamespace($class): string
+	{
+		return (new \ReflectionClass($class))->getShortName();
+	}
+}
---
+++ tests/tests/Helper/Stubs/Folder/ClassWithAssociatedEntity.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Joomla! entity library.
+ *
+ * @copyright  Copyright (C) 2017-2019 Roberto Segura López, Inc. All rights reserved.
+ * @license    See COPYING.txt
+ */
+
+namespace Phproberto\Joomla\Entity\Tests\MVC;
+
+defined('_JEXEC') || die;
+
+use Phproberto\Joomla\Entity\Entity;
+
+/**
+ * Sample class to test AssociatedEntityClass trait.
+ *
+ * @since  __DEPLOY_VERSION__
+ */
+class ClassWithAssociatedEntity
+{
+}
